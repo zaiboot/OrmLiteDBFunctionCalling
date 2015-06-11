319e42dc89dcf70484eccef60695403668eb0282
Zaiboot <zaiboot@gmail.com>
2015-06-11 14:23:44 -0600

    Adding constr entry

--- POC.Repository.Tests/Repositories/EntityRepositoryTests.cs
+++ POC.Repository.Tests/Repositories/EntityRepositoryTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Configuration;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using POC.Repository.Repositories;
+using ServiceStack.OrmLite;
+using Trintech.Cadency.DataAccess.Core;
 
 
 namespace POC.Repository.Tests.Repositories
@@ -10,9 +13,19 @@ namespace POC.Repository.Tests.Repositories
     [ExcludeFromCodeCoverage]
     public class EntityRepositoryTests
     {
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            OrmLiteConfig.DialectProvider = SqlServerDialect.Provider;
+            
+        }
         [TestMethod]
         public void GetListTest()
         {
+            IDbConnectionFactory dbConnectionFactory = new OrmLiteConnectionFactory(ConfigurationManager.ConnectionStrings["constr"].ConnectionString);
+            ICadencyOrm cadencyOrm = new CadencyOrmLite(dbConnectionFactory);
+            CadencyOrm.Initialize(cadencyOrm);
             var repo = new EntityRepository();
             var list = repo.GetList();
             Assert.IsNotNull(list);
