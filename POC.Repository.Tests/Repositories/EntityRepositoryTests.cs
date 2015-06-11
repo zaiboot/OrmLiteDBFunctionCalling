aa99c655df37615ebdc6152efcc848e6faa86370
Zaiboot <zaiboot@gmail.com>
2015-06-11 13:26:35 -0600

    Adding unit tests

---
+++ POC.Repository.Tests/Repositories/EntityRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POC.Repository.Repositories;
+
+
+namespace POC.Repository.Tests.Repositories
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class EntityRepositoryTests
+    {
+        [TestMethod]
+        public void GetListTest()
+        {
+            var repo = new EntityRepository();
+            var list = repo.GetList();
+            Assert.IsNotNull(list);
+        }
+    }
+}
